add2a51d077bab3d873ce86c18690c15f3ec0e8f
K2adir <k2adir@gmail.com>
2023-03-01 11:43:20 -0500

    const

---
+++ components/constant.js
@@ -0,0 +1,12 @@
+let playerHand = [];
+let computerHand = [];
+//
+let currentPlayerCards = 0;
+let currentComputerCards = 0;
+
+let bank = 100;
+let bet = 0;
+let betMulti = 0;
+
+const winMulti = 2;
+const blackJackMulti = 3;
