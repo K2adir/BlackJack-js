868f435927b046cab4f014bcf7bbd0bc4aa53471
K2adir <k2adir@gmail.com>
2023-03-01 13:44:37 -0500

    can create face Card

--- components/createDeck.js
+++ components/createDeck.js
@@ -5,8 +5,23 @@ const createDeck = (suit) => {
     let cardWord = "card";
     let cardNumber = 1;
     let card = cardNumber + cardWord;
+    // THIS NEEDS IF STATEMENT, IT CAN EQUAL TO 1 OR 11;
     card = new Card("A", suit, 11);
     deck.push(card);
+  }
+
+  function crateFace(suit) {
+    // Card(LETTER, SUIT, VALUE)
+    // J
+    let card = "card" + 11;
+    card = new Card("J", suit, 10);
+    deck.push(card);
+    // Q
+    card = "card" + 12;
+    card = new Card("Q", suit, 10);
+    // K
+    card = "card" + 13;
+    card = new Card("K", suit, 10);
   }
 };
 
