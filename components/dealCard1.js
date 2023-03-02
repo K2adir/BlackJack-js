3b183ce81930322225a831d78ba614d7ba2d8665
K2adir <k2adir@gmail.com>
2023-03-02 17:43:22 -0500

    card flips

--- components/dealCard1.js
+++ components/dealCard1.js
@@ -6,4 +6,28 @@ function dealFirstCards() {
   // Dealer is dealt two cards
   dealerCards.push(randomCard(), randomCard());
   currentComputerCards = 2;
+
+  showHUD();
+
+  // first card animation , FANCY
+  addCardtoPlayer(1);
+
+  setTimeout(function () {
+    return addCardtoPlayer(2);
+  }, cardDealingTimeout);
+
+  setTimeout(function () {
+    return addCardtoDealer(1);
+  }, cardDealingTimeout * 2);
+
+  // Flipped card
+  const secondCardDealerFlipped = document.createElement("li");
+
+  secondCardDealerFlipped.classList.add("card", "flipped", "undealed_dealer");
+
+  secondCardDealerFlipped.setAttribute("id", "dealer_card");
+
+  secondCardDealerFlipped.innerHTML = "<h3></h3>";
+
+  document.getElementById("dealer_hand").appendChild(secondCardDealerFlipped);
 }
