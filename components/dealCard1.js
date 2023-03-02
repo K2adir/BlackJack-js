0c796c97a7bf628692ea31c0ee74c612f4038545
K2adir <k2adir@gmail.com>
2023-03-02 17:51:29 -0500

    checks ACE and 21

--- components/dealCard1.js
+++ components/dealCard1.js
@@ -1,26 +1,21 @@
 function dealFirstCards() {
-  // Player is dealt two cards
+  // Player and computer got two cards
   playerCards.push(randomCard(), randomCard());
   currentPlayerCards = 2;
-
-  // Dealer is dealt two cards
   dealerCards.push(randomCard(), randomCard());
   currentComputerCards = 2;
 
   showHUD();
 
-  // first card animation , FANCY
+  //  first cards animation
   addCardtoPlayer(1);
 
-  setTimeout(function () {
-    return addCardtoPlayer(2);
-  }, cardDealingTimeout);
+  setTimeout(() => addCardtoPlayer(2), cardDealingTimeout);
 
-  setTimeout(function () {
-    return addCardtoDealer(1);
-  }, cardDealingTimeout * 2);
+  setTimeout(() => addCardtoDealer(1), cardDealingTimeout * 2);
 
-  // Flipped card
+  ////
+  // For the flipped card
   const secondCardDealerFlipped = document.createElement("li");
 
   secondCardDealerFlipped.classList.add("card", "flipped", "undealed_dealer");
@@ -30,4 +25,23 @@ function dealFirstCards() {
   secondCardDealerFlipped.innerHTML = "<h3></h3>";
 
   document.getElementById("dealer_hand").appendChild(secondCardDealerFlipped);
+
+  appendCardDealerAnimation();
+
+  // Check  Ace
+  if (totalValue(playerCards) > 21 && hasAnAce(playerCards)) {
+    turnAceToOne(playerCards);
+    document
+      .querySelector("#ace_becomes_one_player")
+      .classList.remove("hidden");
+  }
+
+  // Check blackjack
+  if (totalValue(playerCards) == 21) {
+    blackjackCheck();
+  } else {
+    // Player's turn to hit or stand
+    playerTurn = true;
+    phase = "doubleDown";
+  }
 }
