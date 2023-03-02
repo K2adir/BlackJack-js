4a021618ef445c0db97e413a00a315f50fb1a5d8
K2adir <k2adir@gmail.com>
2023-03-02 16:51:44 -0500

    setBet function

--- components/gameFunction.js
+++ components/gameFunction.js
@@ -15,4 +15,46 @@ function setBet() {
       refreshBetHUD();
     }
   });
+  window.addEventListener("keydown", function (e) {
+    if (!betSetted && e.key === 38) {
+      increaseBet();
+      refreshBetHUD();
+    }
+  });
+
+  document
+    .querySelector("#button_less_bet")
+    .addEventListener("click", function () {
+      if (!betSetted) {
+        decreaseBet();
+        refreshBetHUD();
+      }
+    });
+
+  window.addEventListener("keydown", function (e) {
+    if (!betSetted && e.key === 40) {
+      decreaseBet();
+      refreshBetHUD();
+    }
+  });
+
+  document
+    .querySelector("#button_set_bet")
+    .addEventListener("click", function () {
+      if (!betSetted) {
+        document.querySelector("#bet_wrapper").classList.add("hidden");
+        document.querySelector("#bet_buttons").classList.add("hidden");
+        dealFirstCards();
+        betSetted = true;
+      }
+    });
+
+  window.addEventListener("keyup", function (e) {
+    if (!betSetted && e.key === 13) {
+      document.getElementById("bet_wrapper").classList.add("hidden");
+      document.getElementById("bet_buttons").classList.add("hidden");
+      dealFirstCards();
+      betSetted = true;
+    }
+  });
 }
