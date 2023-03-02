0569e9b52fddddc6620032ae87bd912d1372a317
K2adir <k2adir@gmail.com>
2023-03-02 17:04:17 -0500

    better readability

--- components/setBet.js
+++ components/setBet.js
@@ -2,57 +2,57 @@ function setBet() {
   let betSetted = false;
   const betWrapper = document.querySelector("#bet_wrapper");
   const betButtons = document.querySelector("#bet_buttons");
+  const buttonMoreBet = document.querySelector("#button_more_bet");
+  const buttonLessBet = document.querySelector("#button_less_bet");
+  const buttonSetBet = document.querySelector("#button_set_bet");
 
+  // Show the bet wrapper and buttons
   betWrapper.classList.remove("hidden");
   betButtons.classList.remove("hidden");
+
   refreshBetHUD();
 
-  const buttonMoreBet = document.querySelector("#button_more_bet");
-
-  buttonMoreBet.addEventListener("click", function () {
+  // Increase bet when
+  buttonMoreBet.addEventListener("click", () => {
     if (!betSetted) {
       increaseBet();
       refreshBetHUD();
     }
   });
-  window.addEventListener("keydown", function (e) {
-    if (!betSetted && e.key === 38) {
+  window.addEventListener("keydown", (e) => {
+    if (!betSetted && e.key === "ArrowUp") {
       increaseBet();
       refreshBetHUD();
     }
   });
 
-  document
-    .querySelector("#button_less_bet")
-    .addEventListener("click", function () {
-      if (!betSetted) {
-        decreaseBet();
-        refreshBetHUD();
-      }
-    });
-
-  window.addEventListener("keydown", function (e) {
-    if (!betSetted && e.key === 40) {
+  // Decrease bet when
+  buttonLessBet.addEventListener("click", () => {
+    if (!betSetted) {
+      decreaseBet();
+      refreshBetHUD();
+    }
+  });
+  window.addEventListener("keydown", (e) => {
+    if (!betSetted && e.key === "ArrowDown") {
       decreaseBet();
       refreshBetHUD();
     }
   });
 
-  document
-    .querySelector("#button_set_bet")
-    .addEventListener("click", function () {
-      if (!betSetted) {
-        document.querySelector("#bet_wrapper").classList.add("hidden");
-        document.querySelector("#bet_buttons").classList.add("hidden");
-        dealFirstCards();
-        betSetted = true;
-      }
-    });
-
-  window.addEventListener("keyup", function (e) {
-    if (!betSetted && e.key === 13) {
-      document.getElementById("bet_wrapper").classList.add("hidden");
-      document.getElementById("bet_buttons").classList.add("hidden");
+  // Set the bet when 'Set Bet'
+  buttonSetBet.addEventListener("click", () => {
+    if (!betSetted) {
+      betWrapper.classList.add("hidden");
+      betButtons.classList.add("hidden");
+      dealFirstCards();
+      betSetted = true;
+    }
+  });
+  window.addEventListener("keyup", (e) => {
+    if (!betSetted && e.key === "Enter") {
+      betWrapper.classList.add("hidden");
+      betButtons.classList.add("hidden");
       dealFirstCards();
       betSetted = true;
     }
