9d3f03ff5f2a4625d97a30b59900c1b030e88a3e
K2adir <k2adir@gmail.com>
2023-03-03 15:04:43 -0500

    cardClass moved to merged file

--- components/merginFiles.js
+++ components/merginFiles.js
@@ -33,3 +33,24 @@ const debounce = (fn, delay) => {
 };
 
 document.addEventListener("contextmenu", (event) => event.preventDefault());
+
+//// card Class
+class Card {
+  constructor(number, suit, value) {
+    this._number = number;
+    this._suit = suit;
+    this._value = value;
+  }
+
+  get suit() {
+    return this._suit;
+  }
+
+  get value() {
+    return this._value;
+  }
+
+  get number() {
+    return this._number;
+  }
+}
