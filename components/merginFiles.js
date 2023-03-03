733dc30b5016fcd2dbc9ed96fb46341f2e491a3c
K2adir <k2adir@gmail.com>
2023-03-03 15:00:09 -0500

    Constants moved to new file ++
    constants moved to new file and timeout added for better UI UX
    
    last eventlistener should prevent rightclick

---
+++ components/merginFiles.js
@@ -0,0 +1,35 @@
+let playerCards = [];
+let dealerCards = [];
+let deck = [];
+
+let currentPlayerCards = 0;
+let currentComputerCards = 0;
+
+let bank = 100;
+let bet = 0;
+let betMulti = 0;
+const winMulti = 2;
+const blackJackMulti = 3;
+
+let gameStarted = false;
+let playerTurn = false;
+let dealerTurn = false;
+let phase = "";
+
+let cardTimeout = 600;
+let bigSignTimeout = 600;
+let cardDealingTimeout = 150;
+
+let keyboardTipCount = 0;
+let blackjackTipCount = 0;
+document.addEventListener("contextmenu", (e) => e.preventDefault);
+
+const debounce = (fn, delay) => {
+  let timer = null;
+  return (...args) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => fn(...args), delay);
+  };
+};
+
+document.addEventListener("contextmenu", (event) => event.preventDefault());
