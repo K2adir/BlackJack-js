1f43b79001d15c70c0d2aa0431f9ad0c07ca4b0c
K2adir <k2adir@gmail.com>
2023-03-04 19:37:46 -0500

    updateBet moved

--- components/merginFiles.js
+++ components/merginFiles.js
@@ -370,4 +370,16 @@ document.addEventListener("DOMContentLoaded", function () {
     checkForBustOrBlackjack();
   }
   
+  function updateBet() {
+    if (bet * 2 <= bank) {
+      bank -= bet;
+      bet *= 2;
+      betMulti = bet;
+    } else {
+      bet += bank;
+      bank = 0;
+      betMulti = bet;
+    }
+  }
+  
 }
