6ebd9315da63a044c12043f44a14f8e3e5dcb990
K2adir <k2adir@gmail.com>
2023-03-04 19:32:16 -0500

    dealFirstCard Moved to merged file

--- components/merginFiles.js
+++ components/merginFiles.js
@@ -295,4 +295,51 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 }
   
+ function dealFirstCards() {
+    // Player is dealt two cards
+    playerCards.push(randomCard(), randomCard());
+    currentPlayerCards = 2;
+
+    // Dealer is dealt two cards
+    dealerCards.push(randomCard(), randomCard());
+    currentComputerCards = 2;
+
+    // Show HUD
+    showHUD();
+
+    addCardtoPlayer(1);
+
+    setTimeout(() => addCardtoPlayer(2), cardDealingTimeout);
+
+    setTimeout(() => addCardtoDealer(1), cardDealingTimeout * 2);
+
+    const secondCardDealerFlipped = document.createElement("li");
+
+    secondCardDealerFlipped.classList.add("card", "flipped", "undealed_dealer");
+
+    secondCardDealerFlipped.setAttribute("id", "dealer_card");
+
+    secondCardDealerFlipped.innerHTML = "<h3></h3>";
+
+    document.getElementById("dealer_hand").appendChild(secondCardDealerFlipped);
+
+    appendCardDealerAnimation();
+
+    if (totalValue(playerCards) > 21 && hasAnAce(playerCards)) {
+      turnAceToOne(playerCards);
+      document
+        .querySelector("#ace_becomes_one_player")
+        .classList.remove("hidden");
+    }
+
+    if (totalValue(playerCards) == 21) {
+      blackjackCheck();
+    } else {
+      // Player's turn to hit or stand
+      playerTurn = true;
+      phase = "doubleDown";
+    }
+  }
+
+
 }
